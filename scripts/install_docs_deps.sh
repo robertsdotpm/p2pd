9ed2e015a1f5a26200367d3d3fdfcf573cc5a95c
Matthew Roberts <matthew@roberts.pm>
2023-07-23 16:01:34 +1000

    docs.

---
+++ scripts/install_docs_deps.sh
@@ -0,0 +1,6 @@
+python3 -m pip install sphinx==6.2.1
+python3 -m pip install sphinx_rtd_theme==1.2.2
+python3 -m pip install myst-parser==2.0.0
+python3 -m pip install sphinx_rtd_theme==1.2.2
+python3 -m pip install readthedocs-sphinx-search==0.3.
+echo "Restart your terminal now!"
