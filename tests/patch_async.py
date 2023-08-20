4b74f3c0d0619c31c0c58a87feb463b140c09fc9
Matthew Roberts <matthew@roberts.pm>
2023-08-20 13:05:45 +1000

    Saving some notes. Already mentally exhausted so time to play some games that are made for the mental equivalent of babies.

---
+++ tests/patch_async.py
@@ -0,0 +1,136 @@
+import weakref
+
+from asyncio.streams import FlowControlMixin
+from asyncio import StreamWriter
+from asyncio import protocols
+from asyncio import coroutines
+
+# Original StreamReaderProtocol class
+
+class PatchTranslate(FlowControlMixin, protocols.Protocol):
+    """Helper class to adapt between Protocol and StreamReader.
+
+    (This is a helper class instead of making StreamReader itself a
+    Protocol subclass, because the StreamReader has other potential
+    uses, and to prevent the user of the StreamReader to accidentally
+    call inappropriate methods of the protocol.)
+    """
+
+    _source_traceback = None
+
+    def __init__(self, stream_reader, client_connected_cb=None, loop=None):
+        super().__init__(loop=loop)
+        if stream_reader is not None:
+            self._stream_reader_wr = weakref.ref(stream_reader)
+            self._source_traceback = stream_reader._source_traceback
+        else:
+            self._stream_reader_wr = None
+        if client_connected_cb is not None:
+            # This is a stream created by the `create_server()` function.
+            # Keep a strong reference to the reader until a connection
+            # is established.
+            self._strong_reader = stream_reader
+        self._reject_connection = False
+        self._stream_writer = None
+        self._task = None
+        self._transport = None
+        self._client_connected_cb = client_connected_cb
+        self._over_ssl = False
+        self._closed = self._loop.create_future()
+
+    @property
+    def _stream_reader(self):
+        if self._stream_reader_wr is None:
+            return None
+        return self._stream_reader_wr()
+
+    def _replace_writer(self, writer):
+        loop = self._loop
+        transport = writer.transport
+        self._stream_writer = writer
+        self._transport = transport
+        self._over_ssl = transport.get_extra_info('sslcontext') is not None
+
+    def connection_made(self, transport):
+        print("connection made called")
+        if self._reject_connection:
+            context = {
+                'message': ('An open stream was garbage collected prior to '
+                            'establishing network connection; '
+                            'call "stream.close()" explicitly.')
+            }
+            if self._source_traceback:
+                context['source_traceback'] = self._source_traceback
+            self._loop.call_exception_handler(context)
+            transport.abort()
+            return
+        self._transport = transport
+        reader = self._stream_reader
+        if reader is not None:
+            reader.set_transport(transport)
+        self._over_ssl = transport.get_extra_info('sslcontext') is not None
+        if self._client_connected_cb is not None:
+            self._stream_writer = StreamWriter(transport, self,
+                                               reader,
+                                               self._loop)
+            res = self._client_connected_cb(reader,
+                                            self._stream_writer)
+            if coroutines.iscoroutine(res):
+                self._task = self._loop.create_task(res)
+            self._strong_reader = None
+
+    def connection_lost(self, exc):
+        print("connection lost called")
+        reader = self._stream_reader
+        if reader is not None:
+            if exc is None:
+                reader.feed_eof()
+            else:
+                reader.set_exception(exc)
+        if not self._closed.done():
+            if exc is None:
+                self._closed.set_result(None)
+            else:
+                self._closed.set_exception(exc)
+        super().connection_lost(exc)
+        self._stream_reader_wr = None
+        self._stream_writer = None
+        self._task = None
+        self._transport = None
+
+    def data_received(self, data):
+        print("data received")
+        reader = self._stream_reader
+        if reader is not None:
+            reader.feed_data(data)
+
+    def eof_received(self):
+        print("eof received")
+        reader = self._stream_reader
+        if reader is not None:
+            reader.feed_eof()
+
+        if self._over_ssl:
+            # Prevent a warning in SSLProtocol.eof_received:
+            # "returning true from eof_received()
+            # has no effect when using ssl"
+            return False
+        return True
+
+    def _get_close_waiter(self, stream):
+        print("get close waited called")
+        return self._closed
+
+    def __del__(self):
+        print("del called")
+        # Prevent reports about unhandled exceptions.
+        # Better than self._closed._log_traceback = False hack
+        try:
+            closed = self._closed
+        except AttributeError:
+            pass  # failed constructor
+        else:
+            if closed.done() and not closed.cancelled():
+                closed.exception()
+
+    
---
+++ tests/test_pipe.py
@@ -0,0 +1,67 @@
+from p2pd.test_init import *
+from p2pd import IPRange, HOST_TYPE_IP, HOST_TYPE_DOMAIN
+from p2pd import IP6, IP4, Route, Interface
+from p2pd.net import VALID_AFS, ip_norm, TCP, UDP, Bind
+from p2pd.echo_server import EchoServer
+from p2pd.base_stream import pipe_open, SUB_ALL
+from p2pd.address import Address
+from p2pd.utils import what_exception
+from p2pd.daemon import Daemon
+
+asyncio.set_event_loop_policy(SelectorEventPolicy())
+class TestPipe(unittest.IsolatedAsyncioTestCase):
+    async def test_connection_close(self):
+        reader = asyncio.StreamReader(limit=10000)
+        class MinReader(PatchTranslate):
+            def __init__(self, reader):
+                self.close_set = False
+                super().__init__(reader)
+
+            def connection_made(self, transport):
+                self.transport = transport
+                super().connection_made(transport)
+
+            def data_received(self, data):
+                print("Received:", data.decode())
+                super().data_received(data)
+
+            def eof_received(self):
+                reader = self._stream_reader
+                if reader is not None:
+                    reader.feed_eof()
+
+                return False
+
+            def connection_lost(self, exc):
+                print("con lost")
+                # The socket has been closed
+                self.close_set = True
+
+        # Make server to test close handler works.
+        loop = asyncio.get_event_loop()
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lsock.bind(('127.0.0.1', 0))
+        print(lsock)
+        mr = MinReader(reader)
+        server = await loop.create_server(
+            lambda: mr,
+            sock=lsock
+        )
+
+        # Connect to server and close pipe.
+        dest = ("127.0.0.1", lsock.getsockname()[1])
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cs.connect(dest)
+        print(cs)
+        cs.close()
+        await asyncio.sleep(3)
+        print(mr.close_set)
+        server.close()
+
+
+if __name__ == '__main__':
+    main()
+
+"""
+echo server tcp stream socket not being closed.
+"""
