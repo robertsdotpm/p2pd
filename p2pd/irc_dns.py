c050ccc13969b347e3fa2d8df73a1ce773462dfe
Matthew Roberts <matthew@roberts.pm>
2023-11-17 06:33:21 +0100

    Basic message extract function.

--- p2pd/irc_dns.py
+++ p2pd/irc_dns.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 from .address import *
 from .interface import *
 from .base_stream import *
@@ -9,6 +10,49 @@ IRC_PORT = 6697
 IRC_CONF = dict_child({
     "use_ssl": 1
 }, NET_CONF)
+
+class IRCMsg():
+    def __init__(self, prefix, cmd, param, suffix):
+        self.prefix = prefix
+        self.cmd = cmd
+        self.param = param
+        self.suffix = suffix
+
+"""
+TCP is stream-oriented and portions of IRC protocol messages
+may be received. This code handles finding valid IRC messages and
+truncating the recv buffer once extracted.
+"""
+def extract_irc_msgs(buf):
+    # optional             optional
+    # :prefix CMD param... :suffix ( :[^\r]+)?
+    p = "(?:[:]([^ :]+?) )?([A-Z0-9]+) (?:([^\r\:]+?) ?)(?:[:]([^:\r]+))?\r\n"
+    p = re.compile(p)
+
+    # Loop over all protocol messages in buf.
+    # Truncate buf as messages are extracted.
+    msgs = []
+    offset = None
+    while 1:
+        # No matches found yet.
+        # Buffer contains a partial response or its empty.
+        match = p.search(buf)
+        if match is None:
+            break
+
+        # Extract matched message.
+        msg = IRCMsg(*list(match.groups()))
+        msgs.append(msg)
+
+        # Get the offset of the match end.
+        # Truncate buf as messages are extracted.
+        offset = match.end()
+        if offset < len(buf):
+            buf = buf[offset + 1:]
+        else:
+            break
+
+    return msgs, buf
 
 class IRCDNS():
     def __init__(self):
@@ -25,8 +69,19 @@ class IRCDNS():
         await pipe.close()
         return self
 
+    async def msg_cb(self):
+        pass
+
 if __name__ == '__main__':
     async def test_irc_dns():
+        msg = """:server.example.com 001 nickname test :Welcome to the IRC server, nickname!\r\n
+:server.example.com 376 nickname :End of MOTD\r\n
+PRIVMSG #channel :Hello, this is a message!\r\n
+"""
+        out = extract_irc_msgs(msg)
+
+
+        return
         i = await Interface().start()
         ircdns = await IRCDNS().start(i)
         pass
