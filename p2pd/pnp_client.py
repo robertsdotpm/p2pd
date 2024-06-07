722babde34b8408e30acfc6807898f7e73411960
Matthew Roberts <matthew@roberts.pm>
2024-06-07 16:08:29 +1000

    Fix imports.

--- p2pd/pnp_client.py
+++ p2pd/pnp_client.py
@@ -1,6 +1,6 @@
 # TODO: different key per request.
 
-from .ecies import generate_key, decrypt
+from .ecies import generate_key, decrypt, encrypt
 from .base_stream import *
 from .pnp_utils import *
 
