3760c7146d1afa802b1d4ee2254f2dc3910e3dc3
Matthew Roberts <matthew@roberts.pm>
2024-07-19 02:41:43 -0400

    Move node utils into own module.

--- p2pd/p2p_node.py
+++ p2pd/p2p_node.py
@@ -8,6 +8,7 @@ from .daemon import *
 from .signaling import *
 from .machine_id import get_machine_id, hashed_machine_id
 from .p2p_utils import *
+from .p2p_node_extra import *
 
 NODE_CONF = dict_child({
     # Reusing address can hide errors for the socket state.
@@ -15,98 +16,6 @@ NODE_CONF = dict_child({
     "reuse_addr": False,
 }, NET_CONF)
 
-class P2PUtils():
-    # Accomplishes port forwarding and pin hole rules.
-    async def forward(self, port):
-        tasks = []
-        for server in self.servers:
-            # Get the bind IP and interface for the route.
-            route = server[0]
-
-            # Only forward to public IPv6 addresses.
-            ipr = IPRange(route.nic())
-            if route.af == IP6 and ipr.is_private:
-                continue
-
-            # Make task to forward this route.
-            task = route.forward(port)
-            tasks.append(task)
-
-        # Get a list of tasks to do forwarding or pin holes.
-        results = await asyncio.gather(*tasks)
-        tasks = []
-        for result in results:
-            if len(result):
-                tasks += result
-
-        # Now do that all at once since it might take a while.
-        if len(tasks):
-            await asyncio.gather(*tasks)
-
-    # Given a dict of credentials find the associated client.
-    def find_turn_client(self, turn_server, af=None, interface=None):
-        for turn_client in self.turn_clients:
-            # Get credentials for turn client.
-            credentials = turn_client.get_turn_server(af=af)
-
-            # Compare credentials to turn_server.
-            is_same = find_turn_server(credentials, [turn_server])
-            if not is_same:
-                continue
-
-            # Not the same interface.
-            if interface is not None:
-                if turn_client.turn_pipe.route.interface != interface:
-                    continue
-
-            # Otherwise found it.
-            return turn_client
-
-        return None
-
-    def find_signal_pipe(self, addr):
-        our_offsets = list(self.signal_pipes)
-        for offset in addr["signal"]:
-            if offset in our_offsets:
-                return self.signal_pipes[offset]
-
-        return None
-
-    def rm_pipe_id(self, pipe_id):
-        def closure(data, client_tup, pipe):
-            # Delete reference to pipe.
-            log(f"Running rm pipe id {pipe_id}")
-            if pipe_id in self.pipes:
-                del self.pipes[pipe_id]
-
-        return closure
-
-    def setup_multiproc(self, pp_executor, mp_manager):
-        # Process pools are disabled.
-        if pp_executor is None:
-            self.pp_executor = None
-            self.mp_manager = None
-            return
-            
-        assert(mp_manager)
-        self.pp_executor = pp_executor
-        self.mp_manager = mp_manager
-
-    def setup_coordination(self, sys_clock):
-        self.sys_clock = sys_clock
-
-    def setup_tcp_punching(self):
-        self.tcp_punch_clients = [
-            TCPPunch(
-                interface,
-                self.ifs,
-                self,
-                self.sys_clock,
-                self.pp_executor,
-                self.mp_manager
-            )
-            for interface in self.ifs
-        ]
 
 # Main class for the P2P node server.
 class P2PNode(Daemon, P2PUtils):
