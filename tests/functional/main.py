1c0318db43dc878a32ab8f73cf83485ae2020feb
Matthew Roberts <matthew@roberts.pm>
2025-11-02 04:09:59 -0500

    Add some SSH shells to start the command-line NAT testing code with.

--- tests/functional/main.py
+++ tests/functional/main.py
@@ -10,8 +10,21 @@ async def run_client():
             path_join = nt_join
 
         p2pd_dir = path_join(*server["home"], "p2pd_dev", "p2pd")
+        port = 22
+        if "port" in server:
+            port = server["port"]
 
-        async with asyncssh.connect(server["ip"], username=server["user"], client_keys=['~/.ssh/id_rsa']) as conn:
+        async with asyncssh.connect(server["ip"], username=server["user"], client_keys=['~/.ssh/id_rsa'], port=port) as conn:
+            #print(conn)
+            #result = await conn.run("echo hello", check=True)
+            #print(result.stdout, end='')
+            #return
+            if "cmd" in server:
+                cmd = server["cmd"]
+                print(cmd)
+                result = await conn.run(cmd)
+                print(result.stdout, end='')
+
             cmd = f"""cd "{p2pd_dir}" && git pull"""
             print(cmd)
             result = await conn.run(cmd, check=True)
--- tests/functional/servers.py
+++ tests/functional/servers.py
@@ -1,22 +1,88 @@
 SSH_SERVERS = [
     {
+        "os": "mac os x big sur",
+        "ip": "10.0.1.158",
+        "user": "xx",
+        "home": ["/", "Users", "xx"],
+    },
+
+    {
+        "os": "android pixel 9a",
+        "ip": "10.0.1.123",
+        "user": "x",
+        "home": [],
+        "port": 8022,
+        #"cmd": "proot-distro login debian"
+    },
+    
+    {
+        "os": "freebsd",
+        "ip": "10.0.1.225",
+        "user": "root",
+        "home": ["/", "root"]
+    },
+
+    {
+        "os": "fedora",
+        "ip": "10.0.1.224",
+        "user": "x",
+        "home": ["/", "home", "x"]
+    },
+
+    {
+        "os": "ghostbsd",
+        "ip": "10.0.1.152",
+        "user": "x",
+        "home": ["/", "home", "x"]
+    },
+
+    {
+        "os": "windows 11",
+        "ip": "10.0.1.123",
+        "user": "matth",
+        "home": ["C:\\", "Users", "matth"]
+    },
+
+    {
+        "os": "windows xp pro",
+        "ip": "10.0.1.132",
+        "user": "matthew",
+        "home": ["C:\\", "Documents and Settings", "matthew"]
+    },
+
+    {
+        "os": "windows 8.1 pro",
+        "ip": "10.0.1.165",
+        "user": "x",
+        "home": ["C:\\", "Users", "x"]
+    },
+
+    {
+        "os": "windows server 2022",
+        "ip": "10.0.1.248",
+        "user": "administrator",
+        "home": ["C:\\", "Users", "Administrator"]
+    },
+
+    {
         "os": "windows vista",
         "ip": "10.0.1.167",
         "user": "x",
+        "home": ["C:\\", "Users", "x"]
+    },
+
+    {
+        "os": "windows 10",
+        "ip": "10.0.1.199",
+        "user": "matth",
         "home": ["C:\\", "Users", "matth"]
-    }
+    },
+
+    {
+        "os": "windows 7",
+        "ip": "10.0.1.231",
+        "user": "x",
+        "home": ["C:\\", "Users", "x"]
+    },
 ]
 
-{
-    "os": "windows 10",
-    "ip": "10.0.1.199",
-    "user": "matth",
-    "home": ["C:\\", "Users", "matth"]
-}
-
-{
-    "os": "windows 7",
-    "ip": "10.0.1.231",
-    "user": "x",
-    "home": ["C:\\", "Users", "x"]
-}
