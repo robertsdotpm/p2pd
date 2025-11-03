1870e1f30edcb288f9f1a69cdbc9221c74ca48c4
Matthew Roberts <matthew@roberts.pm>
2025-11-02 22:23:39 -0500

    Add python version info.

--- tests/functional/servers.py
+++ tests/functional/servers.py
@@ -1,9 +1,18 @@
 SSH_SERVERS = [
     {
+        "os": "debian",
+        "ip": "110.0.1.251",
+        "user": "x",
+        "home": ["/", "home", "x"],
+        "pyenv": ["3.5.10", "3.7.10", "3.9.10", "3.12.10"]
+    },
+
+    {
         "os": "mac os x big sur",
         "ip": "10.0.1.158",
         "user": "xx",
         "home": ["/", "Users", "xx"],
+        "pyenv": ["3.5.10", "3.7.10", "3.9.10", "3.12.10"]
     },
 
     {
@@ -12,77 +21,88 @@ SSH_SERVERS = [
         "user": "x",
         "home": [],
         "port": 8022,
+        "py": "3.12.12"
         #"cmd": "proot-distro login debian"
     },
-    
+
     {
         "os": "freebsd",
         "ip": "10.0.1.225",
         "user": "root",
-        "home": ["/", "root"]
+        "home": ["/", "root"],
+        "pyenv": ["3.5.10", "3.7.10", "3.9.10", "3.12.10"]
     },
 
     {
         "os": "fedora",
         "ip": "10.0.1.224",
         "user": "x",
-        "home": ["/", "home", "x"]
+        "home": ["/", "home", "x"],
+        "pyenv": ["3.5.3", "3.7.0", "3.9.0", "3.12.0"]
     },
 
     {
         "os": "ghostbsd",
         "ip": "10.0.1.152",
         "user": "x",
-        "home": ["/", "home", "x"]
+        "home": ["/", "home", "x"],
+        "pyenv": ["3.5.10", "3.7.10", "3.9.10", "3.12.10"]
     },
 
     {
         "os": "windows 11",
         "ip": "10.0.1.123",
         "user": "matth",
-        "home": ["C:\\", "Users", "matth"]
+        "home": ["C:\\", "Users", "matth"],
+        "pyenv": ["3.5.0", "3.7.0", "3.9.0", "3.12.0"]
     },
 
     {
         "os": "windows xp pro",
         "ip": "10.0.1.132",
         "user": "matthew",
-        "home": ["C:\\", "Documents and Settings", "matthew"]
+        "home": ["C:\\", "Documents and Settings", "matthew"],
+        "py": "3.5.0"
     },
 
     {
         "os": "windows 8.1 pro",
         "ip": "10.0.1.165",
         "user": "x",
-        "home": ["C:\\", "Users", "x"]
+        "home": ["C:\\", "Users", "x"],
+        "pyenv": ["3.5.0", "3.7.0", "3.9.0", "3.11.0b4"]
     },
 
     {
         "os": "windows server 2022",
         "ip": "10.0.1.248",
         "user": "administrator",
-        "home": ["C:\\", "Users", "Administrator"]
+        "home": ["C:\\", "Users", "Administrator"],
+        "pyenv": ["3.5.0", "3.7.0", "3.9.0", "3.12.0"]
     },
 
     {
         "os": "windows vista",
         "ip": "10.0.1.167",
         "user": "x",
-        "home": ["C:\\", "Users", "x"]
+        "home": ["C:\\", "Users", "x"],
+        "py": "3.7.0"
     },
 
     {
         "os": "windows 10",
         "ip": "10.0.1.199",
         "user": "matth",
-        "home": ["C:\\", "Users", "matth"]
+        "home": ["C:\\", "Users", "matth"],
+        "pyenv": ["3.5.0", "3.7.0", "3.9.0", "3.11.0b4"]
     },
 
     {
         "os": "windows 7",
         "ip": "10.0.1.231",
         "user": "x",
-        "home": ["C:\\", "Users", "x"]
+        "home": ["C:\\", "Users", "x"],
+        "pyenv": ["3.5.0", "3.7.0"]
     },
 ]
 
