2edc22c3f02ac1aa614723629d97cbba96adc5b0
Matthew Roberts <matthew@roberts.pm>
2025-11-09 21:31:44 -0500

    exec coro old.

--- src/p2pd/demo/__main__.py
+++ src/p2pd/demo/__main__.py
@@ -149,6 +149,6 @@ async def main():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     try:
-        asyncio.run(main())
+        loop.run_until_complete(main())
     except KeyboardInterrupt:
         print("keyboard interrupt")
