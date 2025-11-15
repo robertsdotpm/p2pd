03e80c57db4727193fdd42def941061b70447b34
Matthew Roberts <matthew@roberts.pm>
2025-11-14 19:55:09 -0500

    Working on upstream server con?

--- src/p2pd/net/pipe/pipe.py
+++ src/p2pd/net/pipe/pipe.py
@@ -30,7 +30,7 @@ from ..address import *
 from ..asyncio.asyncio_patches import *
 from ..asyncio.async_run import *
 from .pipe_tcp_events import *
-from ..socket import *
+from ..bind.socket import *
 from .pipe_defs import *
 
 class PipeError(Exception):
@@ -79,6 +79,7 @@ class Pipe:
     async def accept(self):
         if self.pipe_events is not None:
             return await self.pipe_events.make_awaitable()
+            
 
     # Pretend to be a pipe_client.
     def __getattr__(self, name):
@@ -139,8 +140,7 @@ class Pipe:
         return asyncio.get_event_loop()
 
     async def resolve_route_and_dest(self):
-        af_hint = getattr(self.route, "af", None)
-        self.route = await self.resolve_route(self.route, af_hint)
+        self.route = await self.resolve_route(self.route)
         self.dest = await self.resolve_dest(self.dest, self.route, self.conf)
 
     async def resolve_route(self, route, af):
@@ -149,7 +149,7 @@ class Pipe:
         Covers the case where a network Interface is passed instead of a route.
         In that case, just use the first available route at first supported AF.
         """
-        if route is not None and getattr(route, "__name__", None) == "Interface":
+        if route is not None and route.__name__ == "Interface":
             nic = route
 
             # For legacy code that passes Interface.
@@ -157,6 +157,7 @@ class Pipe:
 
         # If no route is set -- load a default interface.
         # This is slow and is used as a fallback.
+        af = route.af if route is not None else None
         if route is None:
             from ...nic.interface import Interface
             nic = await Interface()
@@ -192,7 +193,7 @@ class Pipe:
                 ip = ipr_norm(ip)
 
             # Standard address class for resolving addresses.
-            dest = Address(ip, port, route.interface, conf=conf)
+            dest = Address(ip, port, conf=conf)
 
         # Ensure address instances are resolved to IPs.
         if isinstance(dest, Address):
@@ -417,3 +418,14 @@ class Pipe:
         self.owns_socket = False
         self._closed = True
 
+async def pipe_workspace():
+    from ...nic.interface import Interface
+    nic = await Interface()
+    route = await nic.route()
+    async with Pipe(TCP, ("example.com", 80), route).session() as pipe:
+        await pipe.send(b"HTTP 1.0\r\nGET /\r\n\r\n")
+        out = await pipe.recv()
+        print(out)
+
+if __name__ == "__main__":
+    async_run(pipe_workspace())
