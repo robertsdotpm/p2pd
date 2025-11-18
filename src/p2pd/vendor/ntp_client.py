76c15666231244a29836d0549c87bcb3327f4878
Matthew Roberts <matthew@roberts.pm>
2025-11-18 01:34:43 -0500

    Change sub pattern.

--- src/p2pd/vendor/ntp_client.py
+++ src/p2pd/vendor/ntp_client.py
@@ -297,7 +297,7 @@ class NTPClient:
         print(dest)
         try:
             pipe = await Pipe(UDP, dest, route).connect()
-            pipe.subscribe(("", dest))
+            pipe.subscribe((None, dest))
 
             # create the request packet - mode 3 is client
             query_packet = NTPPacket(
