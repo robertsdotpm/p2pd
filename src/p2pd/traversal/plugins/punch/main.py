7c29e9b5a3f27bbafe51d8b468d12ed6afec44c1
Matthew Roberts <matthew@roberts.pm>
2025-12-08 18:31:39 -0500

    Reintroduce pickler

--- src/p2pd/traversal/plugins/punch/main.py
+++ src/p2pd/traversal/plugins/punch/main.py
@@ -60,14 +60,13 @@ class PunchPlugin(TraversalPlugin):
         await asyncio.sleep(3)
         print("delay start punching proc.")
 
-        """
+
         bad = find_unpicklable(puncher)
         if bad:
             path, value, error = bad
             print("Unpicklable at:", path)
             print("Type:", type(value))
             print("Error:", error)
-        """
 
 
         pipe = await start_punching_process(nic, puncher, self.proc_pool)
