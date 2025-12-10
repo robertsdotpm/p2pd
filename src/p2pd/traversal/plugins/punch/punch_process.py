81894d9d495b0875dd48d1cad72b8539274bad45
Matthew Roberts <matthew@roberts.pm>
2025-12-09 20:55:07 -0500

    For real this time. For real.

--- src/p2pd/traversal/plugins/punch/punch_process.py
+++ src/p2pd/traversal/plugins/punch/punch_process.py
@@ -71,7 +71,13 @@ async def start_punching_process(nic, puncher, proc_pool=None):
 
         # Close original listen server.
         # Client pipe is still connected so this is fine.
-        #await listen_pipe.close()
+
+        """
+        I think the issue with this is it does call code to cleanup the
+        client socks so I guess lets fix that.
+        """
+        listen_pipe.tcp_clients = []
+        await listen_pipe.close()
 
         print("return pipe = ", listen_client_pipe)
         #pipe = sock_to_pipe(sock, nic)
