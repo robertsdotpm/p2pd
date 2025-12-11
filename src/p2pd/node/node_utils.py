2d27d5f3479844ccc637e713a9287a8c4c2e994a
Matthew Roberts <matthew@roberts.pm>
2025-12-11 02:06:27 -0500

    Concurrent load stun clients.

--- src/p2pd/node/node_utils.py
+++ src/p2pd/node/node_utils.py
@@ -115,23 +115,32 @@ async def close_idle_pipes(node):
         await asyncio.sleep(min(next_sleep, 5))
 
 async def load_stun_clients(node, limit=USE_MAP_NO):
-    # Already loaded.
     if hasattr(node, "stun_clients"):
         return
-    
+
     node.stun_clients = {IP4: {}, IP6: {}}
-    for if_index in range(0, len(node.ifs)):
+    tasks = []
+
+    for if_index in range(len(node.ifs)):
         interface = node.ifs[if_index]
         for af in interface.supported():
-            node.stun_clients[af][if_index] = await get_n_stun_clients(
-                af=af,
-                n=limit,
-                interface=interface,
-                proto=TCP,
-                conf=PUNCH_CONF,
-            )
+            async def job(af=af, if_index=if_index, interface=interface):
+                clients = await get_n_stun_clients(
+                    af=af,
+                    n=limit,
+                    interface=interface,
+                    proto=TCP,
+                    conf=PUNCH_CONF,
+                )
+                return (af, if_index, clients)
+
+            tasks.append(asyncio.create_task(job()))
+
+    results = await asyncio.gather(*tasks, return_exceptions=False)
+    for af, if_index, clients in results:
+        node.stun_clients[af][if_index] = clients
 
 async def setup_punch_coordination(node, sys_clock):
-    #node.max_punchers, node.pp_executor = await get_pp_executors()
-    node.max_punchers, node.pp_executor = 10, None
+    node.max_punchers, node.pp_executor = await get_pp_executors()
+    #node.max_punchers, node.pp_executor = 10, None
     node.sys_clock = sys_clock
