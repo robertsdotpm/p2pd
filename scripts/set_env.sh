359f580002839064283d050d98958937c715dc3e
Matthew Roberts <matthew@roberts.pm>
2025-12-30 18:31:52 -0500

    It shall be dissected and eaten.

--- scripts/set_env.sh
+++
@@ -1,5 +0,0 @@
-export P2PD_DEBUG=0
-export PNP_DB_PW=""
-export PNP_ENC_PK=""
-export PNP_ENC_SK=""
-
--- tests/test_hello_world.py
+++
@@ -1,6 +0,0 @@
-import uuid
-from p2pd import *
-
-class TestHelloWorld(unittest.IsolatedAsyncioTestCase):
-    async def test_hello_world(self):
-        print("hello")
