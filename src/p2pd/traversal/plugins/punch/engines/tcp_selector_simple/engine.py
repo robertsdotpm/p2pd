b8c737e22e319cdcc6705e3b525d21674aeacb03
Matthew Roberts <matthew@roberts.pm>
2026-01-28 02:15:05 -0500

    Save connect infos.

--- src/p2pd/traversal/plugins/punch/engines/tcp_selector_simple/engine.py
+++ src/p2pd/traversal/plugins/punch/engines/tcp_selector_simple/engine.py
@@ -116,6 +116,7 @@ def tcp_selector_punch_engine(af, nic_id, port_allocs, src_ip, dest_ip, f_sleep_
     # Make outbound connections to the designated ports.
     print("dest ip = ", dest_ip)
     connect_infos = connect_on_tcp_sockets(sel, pre_connect_infos, dest_ip)
+    print("connect infos = ", connect_infos)
 
     # Return set of successful connections (if any.)
     inbound, outbound = socket_event_monitor(sel)
@@ -130,5 +131,5 @@ def tcp_selector_punch_engine(af, nic_id, port_allocs, src_ip, dest_ip, f_sleep_
     # TODO: Not too sure this code is ideal
     # Might need to just send a header and look for it on the other side.
     sock = choose_winning_tcp_sock(dest_ip, sock_list, our_ip)
-    
+
     return sock
